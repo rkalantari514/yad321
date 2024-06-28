62676116958c8c095f8e9fc141d5e6e6c33797e1
rkalantari514 <73887507+rkalantari514@users.noreply.github.com>
2024-06-28 11:13:05 +0330

    eitaa 9

--- yad/views.py
+++ yad/views.py
@@ -35,12 +35,12 @@ def Send_eitaa(request, *args, **kwargs):
     try:
         yadbood = Yad.objects.filter(id=selected_yad_id).first()
         from eitaa import Eitaa
-        token = "1bot19575:9926ae4d-395b-4aea-a412-467fbae01c65"
-        e = Eitaa(token)
-        cap=yadbood.title+" "+yadbood.name+" "+yadbood.family
-        repo.append(cap)
-        file=yadbood.master_image.file
-        repo.append(file)
+        # token = "1bot19575:9926ae4d-395b-4aea-a412-467fbae01c65"
+        # e = Eitaa(token)
+        # cap=yadbood.title+" "+yadbood.name+" "+yadbood.family
+        # repo.append(cap)
+        # file=yadbood.master_image.file
+        # repo.append(file)
 
 
         # filename = settings.MEDIA_ROOT + '/yadbod/None.jpg'
