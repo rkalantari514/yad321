6b80cf9727534cc3b43e4001ffd12bd407033e16
rkalantari514 <73887507+rkalantari514@users.noreply.github.com>
2024-06-28 11:19:03 +0330

    eitaa 10

--- yad/views.py
+++ yad/views.py
@@ -33,8 +33,8 @@ def Send_eitaa(request, *args, **kwargs):
     repo=[]
 
     try:
-        yadbood = Yad.objects.filter(id=selected_yad_id).first()
-        from eitaa import Eitaa
+        # yadbood = Yad.objects.filter(id=selected_yad_id).first()
+        # from eitaa import Eitaa
         # token = "1bot19575:9926ae4d-395b-4aea-a412-467fbae01c65"
         # e = Eitaa(token)
         # cap=yadbood.title+" "+yadbood.name+" "+yadbood.family
