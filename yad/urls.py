86530b964ff19d803cb431a344fb001c0f7a7cf1
rkalantari514 <73887507+rkalantari514@users.noreply.github.com>
2024-06-28 15:34:18 +0330

    eitaa 19

--- yad/urls.py
+++ yad/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from yad.views import YadbodList, Yad_detail, add_salavat, add_100_salavat, Yad_detail_2, add_fatehe, reyad, CreateYad, \
     load_cities2, \
     EditYad, DeleteYad, yad_for_state, yad_for_city, SearchYadView, Yad_Total_Random, Yad_Total_Salavat, \
-    Yad_Total_Fatehe, Yad_Total_Quran, Help, Send_eitaa
+    Yad_Total_Fatehe, Yad_Total_Quran, Help
 
 # path('yadbod' ,YadbodList.as_view()),
 
@@ -12,7 +12,7 @@ from yad.views import YadbodList, Yad_detail, add_salavat, add_100_salavat, Yad_
 urlpatterns = [
     path('' ,YadbodList.as_view(),name="yadbodl"),
     path('help', Help, name="help"),
-    path('sendeitaa', Send_eitaa, name="Sendeitaa"),
+    # path('sendeitaa', Send_eitaa, name="Sendeitaa"),
     path('yadbood/<yadId>', Yad_detail_2, name="yadboodd"),
     path('yadbood/state/<stateId>/<page>', yad_for_state, name="yad_for_state"),
     path('yadbood/city/<cityId>/<page>', yad_for_city, name="yad_for_city"),
