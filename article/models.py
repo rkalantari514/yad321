bfd00b693ca041d4ee67eaf6bf2249391fcfe5b6
rkalantari-hp <rkalantari514@gmail.com>
2024-09-12 17:54:24 +0330

    'article'- add article model / kind-text-imge

--- article/models.py
+++ article/models.py
@@ -1,8 +1,32 @@
+import os
 from django.db import models
 
-# Create your models here.
+
+
+
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+def upload_article_image_path(instance, filename):
+    name, ext = get_filename_ext(filename)
+    final_name = f"{instance.id}{ext}"
+    return f"article/{final_name}"
+
+
+
+
+
+
 class Article(models.Model):
     title  = models.CharField(max_length=150,null=True,blank=True, verbose_name='موضوع')
+    article_kinde  = models.CharField(max_length=150,null=True,blank=True, verbose_name='نوع')
+    article_text = models.TextField(verbose_name='متن مقاله', null=True, blank=True)
+    article_imge = models.ImageField(upload_to=upload_article_image_path, null=True, blank=True, verbose_name='تصویر مقاله')
+
+
 
 
     class Meta:
