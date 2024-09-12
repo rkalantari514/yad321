485dccf6c796bfae80fc9f18361bd7db9aac2ea5
rkalantari-hp <rkalantari514@gmail.com>
2024-09-12 17:36:05 +0330

    'article'- add article model 2

--- article/models.py
+++ article/models.py
@@ -4,5 +4,10 @@ from django.db import models
 class Article(models.Model):
     title  = models.CharField(max_length=150,null=True,blank=True, verbose_name='موضوع')
 
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
+
     def __str__(self):
         return self.title
