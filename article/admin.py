65b488c0f2a94ec57981f287ca8dfbc02ff1eaf4
rkalantari-hp <rkalantari514@gmail.com>
2024-09-12 16:48:52 +0330

    'article'- add article model

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from article.models import Article
+
+
 # Register your models here.
+class Article_Admin(admin.ModelAdmin):
+    list_display = ['__str__']
+
+    class Meta:
+        model = Article
+
+
+
+
+admin.site.register(Article,Article_Admin)
